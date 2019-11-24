90575ef251e8cacb270000f746db9adb201585ae
arindam7development <arindam5191@gmail.com>
2019-11-24 23:36:19 +0530

    user password db hash

--- complex-app/models/User.js
+++ complex-app/models/User.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs")
 const usersCollection = require('../db').collection("users")
 const validator = require("validator")
 
@@ -24,10 +25,25 @@ User.prototype.validate = function() {
   if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {this.errors.push("Username can only contain letters and numbers.")}
   if (!validator.isEmail(this.data.email)) {this.errors.push("You must provide a valid email address.")}
   if (this.data.password == "") {this.errors.push("You must provide a password.")}
-  if (this.data.password.length > 0 && this.data.password.length < 7) {this.errors.push("Password must be at least 7 characters.")}
-  if (this.data.password.length > 100) {this.errors.push("Password cannot exceed 100 characters.")}
+  if (this.data.password.length > 0 && this.data.password.length < 12) {this.errors.push("Password must be at least 12 characters.")}
+  if (this.data.password.length > 50) {this.errors.push("Password cannot exceed 50 characters.")}
   if (this.data.username.length > 0 && this.data.username.length < 3) {this.errors.push("Username must be at least 3 characters.")}
   if (this.data.username.length > 30) {this.errors.push("Username cannot exceed 30 characters.")}
+}
+
+User.prototype.login = function() {
+  return new Promise((resolve, reject) => {
+    this.cleanUp()
+    usersCollection.findOne({username: this.data.username}).then((attemptedUser) => {
+      if (attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)) {
+        resolve("Congrats!")
+      } else {
+        reject("Invalid username / password.")
+      }
+    }).catch(function() {
+      reject("Please try again later.")
+    })
+  })
 }
 
 User.prototype.register = function() {
@@ -38,23 +54,11 @@ User.prototype.register = function() {
   // Step #2: Only if there are no validation errors 
   // then save the user data into a database
   if (!this.errors.length) {
+    // hash user password
+    let salt = bcrypt.genSaltSync(10)
+    this.data.password = bcrypt.hashSync(this.data.password, salt)
     usersCollection.insertOne(this.data)
   }
-}
-
-User.prototype.login = function() {
-  return new Promise((resolve, reject) => {
-    this.cleanUp()
-    usersCollection.findOne({username: this.data.username}).then((attemptedUser) => {
-      if (attemptedUser && attemptedUser.password == this.data.password) {
-        resolve("Congrats!")
-      } else {
-        reject("Invalid username / password.")
-      }
-    }).catch(function() {
-      reject("Please try again later.")
-    })
-  })
 }
 
 module.exports = User
